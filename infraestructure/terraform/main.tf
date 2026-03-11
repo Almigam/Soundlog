91d45b94ac98af50002d652b1127dd3c0f2bd1f3
Miguel <miguelbercedofontecha@gmail.com>
2026-03-11 14:46:44 +0100

    Lanzado Terraform innit, plan y apply para crear el resource group que gestiona la copia de estado de Terraform en la nube

--- infraestructure/terraform/main.tf
+++ infraestructure/terraform/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_resource_group" "main" {
   name     = "rg-soundlog-dev"
-  location = "westeurope"
+  location = "francecentral"
 
   tags = {
     project    = "soundlog"
     managed_by = "terraform"
   }
-}
+}c
