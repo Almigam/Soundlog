d0513215dfc120fa5183c19250b2fabc11ef4d98
Miguel <miguelbercedofontecha@gmail.com>
2026-03-12 18:37:43 +0100

    CREADO: Recurso Azure Blob Storage par la parte de frontend estático.      -Añadida cuenta de almacenamiento(soundlogdevfrontend) dentro de francecentra         -Puesto Hosting web estático con index.html y 404.html                                -Modificado main.tf(Poner el grupo de recursos nuevo) y outputs.tf(enseñar la URL pública del frontend de soundlog)

--- infraestructure/terraform/main.tf
+++ infraestructure/terraform/main.tf
@@ -6,4 +6,26 @@ resource "azurerm_resource_group" "main" {
     project    = "soundlog"
     managed_by = "terraform"
   }
-}c
+}
+
+resource "azurerm_storage_account" "frontend" {
+  name = "soundlogdevfrontend"
+  resource_group_name = azuerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  account_tier= "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+
+  enable_https_traffic_only = true
+  min_tls_version = "TLS1_2"
+
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
+
+  tags = {
+    project    = "soundlog"
+    managed_by = "terraform"
+  }
+}
--- infraestructure/terraform/outputs.tf
+++ infraestructure/terraform/outputs.tf
@@ -8,3 +8,8 @@ output "resource_group_location" {
   description = "Región donde se desplegó el Resource Group"
   value       = azurerm_resource_group.main.location
 }
+
+output "frontend_url" {
+  description = "URL del sitio web estático desplegado en Azure Storage"
+  value       = azurerm_storage_account.frontend.primary_web_endpoint
+}
