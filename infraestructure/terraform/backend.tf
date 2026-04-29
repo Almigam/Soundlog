bc013cf3754d940904598e623c4b221753db6c7b
Miguel <miguelbercedofontecha@gmail.com>
2026-04-29 16:11:44 +0200

    Añadidos los cambios necesarios para completar la configuración de una managed identity y utilizarla para poder desplegar recursos con Terraform desde Github

--- infraestructure/terraform/backend.tf
+++ infraestructure/terraform/backend.tf
@@ -5,5 +5,10 @@ terraform {
     storage_account_name = "soundlogtfstate"   
     container_name       = "tfstate"
     key                  = "soundlog.terraform.tfstate"
+    use_oidc              = true
+    use_azuread_auth      = true
+    tenant_id             = "78f3a279-48c8-4670-9162-a63c451c9fae"   # tu tenant real
+    client_id             = "63dc30b6-de60-4833-8c4b-85cf39ae167c"   # clientId de la Managed Identity
   }
 }
+
