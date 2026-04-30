49354d7a53954c3a67b166027142c2198547e06e
Miguel <miguelbercedofontecha@gmail.com>
2026-04-30 16:55:56 +0200

    Arreglados los formatos incorrectos de terraform con el comando terraform fmt -recursive

--- infraestructure/terraform/main.tf
+++ infraestructure/terraform/main.tf
@@ -9,18 +9,18 @@ resource "azurerm_resource_group" "main" {
 }
 #Frontend estático
 resource "azurerm_storage_account" "frontend" {
-  name = "soundlogdevfrontend"
-  resource_group_name = azurerm_resource_group.main.name
-  location = azurerm_resource_group.main.location
-  account_tier= "Standard"
+  name                     = "soundlogdevfrontend"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind = "StorageV2"
+  account_kind             = "StorageV2"
 
   https_traffic_only_enabled = true
-  min_tls_version = "TLS1_2"
+  min_tls_version            = "TLS1_2"
 
   static_website {
-    index_document = "index.html"
+    index_document     = "index.html"
     error_404_document = "404.html"
   }
 
@@ -32,15 +32,15 @@ resource "azurerm_storage_account" "frontend" {
 
 #Storage account para las imágenes: Fotos perfil y portadas álbumes, singles
 resource "azurerm_storage_account" "images" {
-  name = "soundlogdevimages"
-  resource_group_name = azurerm_resource_group.main.name
-  location = azurerm_resource_group.main.location
-  account_tier= "Standard"
+  name                     = "soundlogdevimages"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind = "StorageV2"
+  account_kind             = "StorageV2"
 
   https_traffic_only_enabled = true
-  min_tls_version = "TLS1_2"
+  min_tls_version            = "TLS1_2"
 
   tags = {
     project    = "soundlog"
@@ -50,15 +50,15 @@ resource "azurerm_storage_account" "images" {
 
 #Contenedores
 resource "azurerm_storage_container" "profile_pictures" {
-  name = "profile-pictures"
-  storage_account_name = azurerm_storage_account.images.name
-  container_access_type = "blob" 
+  name                  = "profile-pictures"
+  storage_account_name  = azurerm_storage_account.images.name
+  container_access_type = "blob"
 }
 
 resource "azurerm_storage_container" "album_covers" {
-  name = "album-covers"
-  storage_account_name = azurerm_storage_account.images.name
-  container_access_type = "blob" 
+  name                  = "album-covers"
+  storage_account_name  = azurerm_storage_account.images.name
+  container_access_type = "blob"
 }
 
 resource "azurerm_mssql_server" "main" {
