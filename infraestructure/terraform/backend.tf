efee2a32de09a47f4e1c8f126e7340ca7df725ff
Miguel <miguelbercedofontecha@gmail.com>
2026-04-30 15:32:48 +0200

    Cambios al tener que cambiar de lugar la Managed Identity a un resource group más seguro rg-soundlog-tfstate

--- infraestructure/terraform/backend.tf
+++ infraestructure/terraform/backend.tf
@@ -8,7 +8,7 @@ terraform {
     use_oidc              = true
     use_azuread_auth      = true
     tenant_id             = "78f3a279-48c8-4670-9162-a63c451c9fae"   # tu tenant real
-    client_id             = "63dc30b6-de60-4833-8c4b-85cf39ae167c"   # clientId de la Managed Identity
+    client_id             = "196dbf1d-e63c-4241-af3d-00cbf4b51a06"   # clientId de la Managed Identity
   }
 }
 
